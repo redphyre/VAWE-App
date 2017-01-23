d26fa219f30118803ad092f14ce7017e0f983ac0
Matthew Walker <matt@elementmedia.design>
2017-01-23 14:10:35 -0600

    updating cordova settings

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "cordova-plugin-armsoft-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-armsoft-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-armsoft-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1"
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-armsoft-statusbar": "1.1.0"
 };
 // BOTTOM OF METADATA
 });
