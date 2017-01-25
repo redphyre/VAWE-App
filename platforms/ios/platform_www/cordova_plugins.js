c44bcb5ce10574a5e84fdcfa27fd62009dc65cfd
Matthew Walker <matt@elementmedia.design>
2017-01-25 09:52:08 -0600

    working on adding touch ID functionality for fingerprint login

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -15,6 +15,14 @@ module.exports = [
         "clobbers": [
             "window.StatusBar"
         ]
+    },
+    {
+        "id": "cordova-plugin-keychain-touch-id.TouchID",
+        "file": "plugins/cordova-plugin-keychain-touch-id/www/touchid.js",
+        "pluginId": "cordova-plugin-keychain-touch-id",
+        "clobbers": [
+            "window.plugins.touchid"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -23,7 +31,8 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.3.1",
     "com.googlemaps.ios": "2.1.1",
     "cordova-plugin-googlemaps": "1.4.0",
-    "cordova-plugin-statusbar": "2.2.1"
+    "cordova-plugin-statusbar": "2.2.1",
+    "cordova-plugin-keychain-touch-id": "3.2.1"
 };
 // BOTTOM OF METADATA
 });
