cb1d1f625ee40e352be56b84122ecd6be6969711
Matthew Walker <matt@elementmedia.design>
2017-02-14 14:55:03 -0600

    updates and additions to messaging system, still in process of programming.

--- www/sendmessage.php
+++ www/sendmessage.php
@@ -1,10 +1,19 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+$message = $_POST['messageToSend'];
+$user = $_POST['user'];
+$vendor = $_POST['vendor'];
+$to_from = "to";
+header("Content-Type: text/html; charset=ISO-8859-1");
+$user = "elementm";
+$pass = "Qazplm10!";
+$dbname = "elementm_wp5";
+$query = "INSERT INTO vawe_messages (vendor, client, message, to_from) VALUES ($vendor, $user, $message, $to_from)";
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=elementm_wp5', $user, $pass);
+	$dbh -> query($query);
+	echo "Success";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
