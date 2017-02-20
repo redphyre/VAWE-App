cd23552134da5a404ad43e09c91407411fc4b5af
Matthew Walker <matt@elementmedia.design>
2017-02-19 19:52:14 -0600

    Added home screen slider with sample ads, and generic ads as well

---
+++ platforms/ios/www/vendorlogin.php
@@ -0,0 +1,44 @@
+<?php
+
+header('content-type: application/json; charset=utf-8');
+header("access-control-allow-origin: *");
+
+include_once('../wp-config.php');
+include_once('../wp-load.php');
+include_once('../wp-includes/wp-db.php');
+include_once('../wp-includes/class-phpass.php');
+
+global $userdata;
+global $wpdb; 
+
+//get the posted values
+
+$posted_username = $_POST['user'];
+$posted_password = $_POST['pass'];
+
+$user_name = htmlspecialchars($posted_username,ENT_QUOTES);
+
+$pass_word = wp_hash_password($posted_password);
+
+$pass_md5 = md5($posted_password);
+
+$pass = $pass_word;
+
+$userinfo = get_userdatabylogin($user_name);
+
+$passcheck = $userinfo->user_pass;
+
+$vendor = $userinfo->vendor_status;
+
+$hash = $userinfo->user_pass;
+$wp_hasher = new PasswordHash(8, TRUE);
+$check = $wp_hasher->CheckPassword($posted_password, $hash);
+if ($vendor == "YES") {
+	session_start();
+	$_SESSION['login'] = "1";
+	$_SESSION['user'] = $user_name; 
+	$_SESSION['vendor'] = "true";
+	echo $vendor;
+}
+else { echo "Username is not listed as a vendor or password is incorrect. Please try again."; }
+?>
